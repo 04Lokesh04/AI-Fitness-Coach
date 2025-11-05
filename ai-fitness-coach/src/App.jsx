74c869fb50697c0cf117afe1c2186f63343dc60a
04Lokesh04 <95993305+04Lokesh04@users.noreply.github.com>
2025-11-05 17:02:26 +0530

    changed file name

--- ai-fitness-coach/src/App.jsx
+++ ai-fitness-coach/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Plans from "./pages/plan";
+import Plans from "./pages/Plans";
 import { ThemeProvider } from "./state/ThemeContext";
 import { PlanProvider } from "./state/PlanContext";
 
